961c51eeb0e42e86002859821269b89172015cff
jivitesh-unencrypted <theunencrypted02@gmailcom>
2026-09-23 22:33:04 +0530

    Allow Render preview host

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     host: true,
+  },
+
+  preview: {
+    allowedHosts: ['smart-agri-survillance.onrender.com'],
   },
 })
